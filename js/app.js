02ef97277748b13150fe53f5d5a5ad751aebce3f
jubee88 <Juliachiang@Cary-Chiangs-iMac.local>
2014-03-23 22:17:17 -0400

    10:17 edit

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
-	//* set variables
-	var currentitem = $("#itemtyped").val();
-	var newlistitem = $("<ul id="actuallist"><input type="checkbox">"+currentitem+"</ul>");
-});
-	//* listen for enter March 22
-		//* append the item to the_list
-	//* remove the list item
-		$("#itemtyped").keyup(function(event){
-		if (event.which===13){
-			$(currentitem).appendTo("newlistitem");
+	//* listen
+		$("#itemtyped").keyup(function(event) {
+	event.preventDefault();
+		if (event.which === 13) {
+			$("#itemtyped").trigger(additem);
 		}
 });
+	var additem = function () {
+		var currentitem = $("#itemtyped").val();
+		var newlistitem = $("<ul id="actuallist"><input type="checkbox">"+currentitem+"</ul>");
+		$(".item").show();
+		$(".item").append(newlistitem);
+		$("#itemtyped").text(" ");
+		$("input").val("");
+};
+});
+	//* remove the list item
